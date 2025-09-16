f8e5896c5cba4c7d406a5b2e51d2d378db345ba3
julian <julianatehortuaz77@gmail.com>
2025-09-15 21:52:57 -0500

    new biomass

--- puntos/uses_cases/biomass_potencial.py
+++ puntos/uses_cases/biomass_potencial.py
@@ -7,9 +7,9 @@ class BiomassPotencial:
 
         n_e = 0.30 # constante del 30%
         pci = 14600 # TODO: este solo es el caso del aguacate, luego toca generar tabla de referencias
-        fgr_agricola = 0.3 #TODO: solo para el aguacate, luego toca generar tabla de referencias
+        fgr_agricola = 0.18 #TODO: solo para el aguacate, luego toca generar tabla de referencias
         mb = a_cultivo * rendimiento * fgr_agricola
-        b_potencial = mb * pci * n_e
+        b_potencial = mb * pci * n_e*1/3600
         print("==="*30)
         print("Variables biomasa")
         print("==="*30)
@@ -30,7 +30,7 @@ class BiomassPotencial:
 
         n_e = 0.30 # constante del 30%
         pci = 18895 # TODO: este solo es el caso de los bovinos, luego toca generar tabla de referencias
-        fgr_pecuario = 7.99 #TODO: solo para bovinos, luego toca generar tabla de referencias
+        fgr_pecuario = 1.59 #TODO: solo para bovinos, luego toca generar tabla de referencias
         mb = cabezas * fgr_pecuario
         b_potencial = mb * pci * n_e
         print("==="*30)
@@ -46,3 +46,6 @@ class BiomassPotencial:
         print("   b_potencial:", b_potencial)
         print("----"*30)
         return b_potencial
+    
+
+    # agrigola el rendimiento es el valor y no 1/ 
